295a94ac9c11fa7e8bb0b48cf151c6d1fb31275c
Mehmet Ali KIZILARSLAN <mehmetali729@gmail.com>
2021-09-21 16:35:22 +0300

    Preparing The Scene
    -Created a player object as a sphere
    -Created a center point empty game object
    -Made the camera child object of center point
    -Created a rotation script for the center point game object assigned to it

---
+++ Last Stand Win/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    private float rotationSpeed = 60.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+
+    }
+}
