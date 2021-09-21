1bada01b698279f2eb9c1ed002dc5472478f74c0
Mehmet Ali KIZILARSLAN <mehmetali729@gmail.com>
2021-09-21 16:53:09 +0300

    Player Movement
    -PlayerController Script is created
    -Player will move forward according to the camera's (center point) rotation.

---
+++ Last Stand Win/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private float speed = 5.0f;
+    private GameObject centerPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        centerPoint = GameObject.Find("CenterPoint");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(centerPoint.transform.forward * forwardInput * speed);
+
+    }
+}
