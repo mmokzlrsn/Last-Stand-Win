7cf755917fc6c9dfffa45ac2838d98fb6ab1c627
Mehmet Ali KIZILARSLAN <mehmetali729@gmail.com>
2021-09-23 14:59:58 +0300

    Spawning more PowerUps
    When new wave comes it will spawn a power up

--- Last Stand Win/Assets/Scripts/SpawnManager.cs
+++ Last Stand Win/Assets/Scripts/SpawnManager.cs
@@ -8,26 +8,32 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9.0f;
     public int enemyCount;
 
+    public GameObject powerUpPrefab;
+
 
     private void Start()
     {
-        
-    
+       
     }
+
     // Update is called once per frame
     void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
 
         if (enemyCount == 0)
+        {
             spawnEnemyWave(Random.Range(1, 4));
+            spawnPowerUp();
+        }
     }
 
-    Vector3 GenerateSpawnPosition()
+
+    Vector3 GenerateSpawnPosition(float yPos)
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 spawnPos = new Vector3(spawnPosX, transform.position.y, spawnPosZ);
+        Vector3 spawnPos = new Vector3(spawnPosX, yPos, spawnPosZ);
 
         return spawnPos;
     }
@@ -36,8 +42,12 @@ public class SpawnManager : MonoBehaviour
     {
         for(int i = 0; i < count; i++)
         {
-            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
-        }
-        
+            Instantiate(enemyPrefab, GenerateSpawnPosition(transform.position.y), enemyPrefab.transform.rotation);
+        }   
+    }
+
+    void spawnPowerUp()
+    {
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(powerUpPrefab.transform.position.y), powerUpPrefab.transform.rotation);
     }
 }
